1ae253317446aa40d453f0eb6a49e6001d3e0ac2
Flechazo <1113553385@qq.com>
2024-10-31 10:23:30 +0800

    completed lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,6 +3,13 @@
  */
 public class LeapYear {
 
+    /** determin if the given year is a leap year
+     *   @param year to be analyzed
+     */
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
